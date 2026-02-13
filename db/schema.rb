c1799b137903acc80a8faabdd609bb8cad3ee72d
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-02-13 21:24:56 +0200

    Delete everything from the signup page, except the form. Convert form into a bootstrap form. Add an additional name field. Wrap the sign up form with a bootstrap container. Add informational text inside the container

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_13_184320) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
