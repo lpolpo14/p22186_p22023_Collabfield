cc7c286c8d3a95878f88f09924c23587be01ce5a
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-02-13 22:50:46 +0200

    - Generate Post and Category models. - Create associations between User, Post and Category models. - Create categories and posts database tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_13_184320) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_13_204532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "branch"
+    t.string "name"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "category_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
