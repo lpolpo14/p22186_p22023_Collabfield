da0df13d2d092cc446dcc7b23ef719acbe467076
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-02-14 22:52:27 +0200

    Added ability to create a new post if logged in (With authentication checks) as well as proper CSS.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  def redirect_if_not_signed_in
+  redirect_to root_path if !user_signed_in?
+  end
+
+  def redirect_if_signed_in
+    redirect_to root_path if user_signed_in?
+  end
+
 end
