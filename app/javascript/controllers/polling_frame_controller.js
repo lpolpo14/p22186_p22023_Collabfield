abe0ffdbef6d7cd0de9d1c47671f2f7775dbe9fe
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-02-17 07:19:34 +0200

    Added Notifications

--- app/javascript/controllers/polling_frame_controller.js
+++ app/javascript/controllers/polling_frame_controller.js
@@ -1,16 +1,59 @@
+// app/javascript/controllers/polling_frame_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static values = { interval: { type: Number, default: 5000 } }
+  static values = { interval: { type: Number, default: 3000 } }
 
   connect() {
+    this.paused = false
+
+    this.bindDropdownListeners()
+    this.start()
+  }
+
+  disconnect() {
+    this.stop()
+    this.unbindDropdownListeners()
+  }
+
+  start() {
+    this.stop()
     this.timer = setInterval(() => {
-      // turbo-frame elements support reload()
+      if (this.paused) return
+
+      // Extra safety: if dropdown is open, don't reload
+      if (this.dropdownIsOpen()) return
+
       this.element.reload()
     }, this.intervalValue)
   }
 
-  disconnect() {
-    clearInterval(this.timer)
+  stop() {
+    if (this.timer) clearInterval(this.timer)
+    this.timer = null
+  }
+
+  bindDropdownListeners() {
+    // The frame content gets replaced, so query inside the frame element
+    this.dropdownToggle = this.element.querySelector('[data-bs-toggle="dropdown"]')
+    if (!this.dropdownToggle) return
+
+    this.onShown = () => { this.paused = true }
+    this.onHidden = () => { this.paused = false }
+
+    this.dropdownToggle.addEventListener("shown.bs.dropdown", this.onShown)
+    this.dropdownToggle.addEventListener("hidden.bs.dropdown", this.onHidden)
+  }
+
+  unbindDropdownListeners() {
+    if (!this.dropdownToggle) return
+    this.dropdownToggle.removeEventListener("shown.bs.dropdown", this.onShown)
+    this.dropdownToggle.removeEventListener("hidden.bs.dropdown", this.onHidden)
+  }
+
+  dropdownIsOpen() {
+    // Bootstrap sets aria-expanded="true" when open
+    return this.element.querySelector('[data-bs-toggle="dropdown"][aria-expanded="true"]') !== null
   }
 }
+
